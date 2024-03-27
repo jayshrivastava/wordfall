514345e34a7f24c39a803c3ef9269132f6990ae8
Jayant Shrivastava <jshrivastava03@gmail.com>
2024-03-26 22:17:24 -0400

    for loop

--- src/main.rs
+++ src/main.rs
@@ -14,27 +14,9 @@ const GRID_HEIGHT: usize = 10;
 
 #[derive(Clone)]
 struct BlockState {
-   val: &'static str,
-    id: u64,
+   val: RwSignal<&'static str>,
+   key: u64,
 }
-
-impl BlockState {
-    // This is a method
-    // `&self` is sugar for `self: &Self`, where `Self` is the type of the
-    // caller object. In this case `Self` = `Rectangle`
-    fn render(&self) -> impl IntoView {
-        let div_class = match self.val {
-            " " =>  styles::block,
-            _ => styles::block_green,
-        };
-       view! {
-           <div class=div_class>
-                <p class=styles::block_text> {self.val} </p>
-           </div>
-       }
-    }
-}
-
 
 // impl IntoView for BlockState {
 //     fn into_view(self) -> View {
@@ -44,26 +26,22 @@ impl BlockState {
 const EMPTY:  &'static str = "_";
 fn new_block_state(val :&'static str) -> BlockState {
     return BlockState{
-        val,
-        id: random(),
+        val: create_rw_signal(val),
+        key: random(),
     }
 }
-//
+
+fn makeBlockVec() -> Vec<BlockState> {
+   let mut ret: Vec<BlockState> = vec![];
+   for _  in 0..GRID_HEIGHT * GRID_HEIGHT {
+       ret.push(new_block_state(EMPTY))
+   }
+    ret
+}
 #[component]
 fn App() -> impl IntoView {
-    let mut set_block: Vec<Vec<WriteSignal<BlockState>>>  = Vec::with_capacity(GRID_HEIGHT);
-    let mut get_block: Vec<Vec<ReadSignal<BlockState>>> = Vec::with_capacity(GRID_HEIGHT) ;
-    for i in 0..GRID_HEIGHT {
-        set_block.push(Vec::with_capacity(GRID_WIDTH));
-        get_block.push(Vec::with_capacity(GRID_WIDTH));
-        for j in 0..GRID_WIDTH {
-            let (get, set) = create_signal(new_block_state(EMPTY));
-            set_block[i].push(set);
-            get_block[i].push(get);
-        }
-    }
-
-    let (current_block, set_current_block) = create_signal(vec![-1,-1]);
+    let (block, setblock) = create_signal(makeBlockVec());
+    let (current_block, set_current_block) = create_signal(0);
 
 
     // let handle = window_event_listener(ev::keypress,move |ev| {
@@ -77,27 +55,25 @@ fn App() -> impl IntoView {
     //
     create_effect(move |_| {
         set_interval(move || {
-            // if current_block()[0] == -1 {
-            //     log!("A");
-            //     set_current_block(vec![0, 5]);
-            //     set_block[0][5](new_block_state(LETTERS[0]))
-            // }
-            log!("B")
+                log!("A");
+                // set_current_block(vec![0]);
+                let _ = block.with(|blocks| {
+                    blocks[0].val.update(|val| *val = "A");
+                log!("{}{}", blocks[0].key, blocks[0].val.get());
+                    log!("{}{}", blocks[1].key, blocks[1].val.get());
+                });
         }, Duration::from_secs(3));
     });
     view! {
-    <div class=styles::grid_container>
-        {get_block.into_iter()
-            .map(|row| view! {
-            <div class=styles::grid_row>
-                {row.into_iter()
-                .map(|block| view! { <div>{block().render()}</div>})
-                .collect_view()}
-            </div>
-        })
-            .collect_view()}
-
-    </div>
+        <For
+            each=block
+            key=|block_state| block_state.key.clone()
+            let: bs
+        >
+        <div>
+           {bs.val}
+        </div>
+        </For>
     }
 
 }
